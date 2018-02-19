983754223eb0fb9923bfa5bbc4a6bdd880e12566
fulibacsi <fulibacsi@gmail.com>
2018-02-19 19:16:33 +0100

    Simplified user interface.

--- app/static/game.js
+++ app/static/game.js
@@ -1,4 +1,11 @@
 
+var ASSETS = {
+    'atk': window.asset_dir + "/atk.png",
+    'def': window.asset_dir + "/def.png",
+    'heal': window.asset_dir + "/heal.png",
+    'buff': window.asset_dir + "/buff.png"
+}
+
 function get_new_id(base='') {
     var id = base;
     var i = 0;
@@ -9,59 +16,54 @@ function get_new_id(base='') {
     return id;
 }
 
-function allowDrop(ev) {
-    ev.preventDefault();
-}
-
-function drag(ev) {
-    var parent = document.getElementById(ev.target.id).parentNode
-    ev.dataTransfer.setData("text", ev.target.id);
-    ev.dataTransfer.setData("parent", parent.id);
-}
-
-function drop(ev) {
-    ev.preventDefault();
-
-    var data = ev.dataTransfer.getData("text");
-    var element = document.getElementById(data);
-    element.setAttribute('onclick', 'remove(id="' + data + '")');
-    var parent_id = ev.dataTransfer.getData("parent");
-    var parent = document.getElementById(parent_id);
-
-    if (ev.target.childNodes.length == 1) {
-        ev.target.appendChild(element);
+function find_target() {
+    if (document.getElementById('move-slot-1').childNodes[0].nodeType == 3) {
+        return document.getElementById('move-slot-1');
+    } else if (document.getElementById('move-slot-2').childNodes[0].nodeType == 3) {
+        return document.getElementById('move-slot-2');
     } else {
-        parent.appendChild(document.getElementById(data));
+        return null;
     }
+}
 
+function move(id) {
+    var target = find_target();
+    if (target != null) {
+        var element = document.getElementById(id);
+        var parent = element.parentNode;
+        element.setAttribute('onclick', 'remove(id="' + id + '")')
+        parent.removeChild(element);
+        parent.innerHTML = "&nbsp;";
+
+        target.innerHTML = '';
+        target.appendChild(element);
+    }
 }
 
 function remove(id) {
     var element = document.getElementById(id);
-    var original = document.getElementById(element.getAttribute('parent-node'));
-    element.parentNode.removeChild(element);
-    element.removeAttribute('onclick');
-    original.appendChild(element);
+    var parent = element.parentNode;
+    element.setAttribute('onclick', 'move(id="' + id + '")')
+    parent.removeChild(element);
+    parent.innerHTML = "&nbsp;";
+
+    var target = document.getElementById(element.getAttribute('parent-node'));
+    target.innerHTML = '';
+    target.appendChild(element);
 }
 
-function create_move(type='', src='', target) {
+function create_move(type='', target) {
+    var id = get_new_id(type + '-icon')
     var img = document.createElement('img');
-    img.setAttribute('src', src);
-    img.setAttribute('id', get_new_id(type + '-icon'));
+    img.setAttribute('src', ASSETS[type]);
+    img.setAttribute('id', id);
     img.setAttribute('width', 50);
-    img.setAttribute('draggable', "true");
-    img.setAttribute('ondragstart', "drag(event)");
+    img.setAttribute('onclick', 'move(id="' + id + '")')
     img.setAttribute('parent-node', target);
 
     var target = document.getElementById(target);
     target.childNodes.forEach( function(child) { target.removeChild(child) });
     target.appendChild(img);
-}
-
-function set_moves() {
-    console.log('Moves setted!');
-    cycle_visibility('p1-place-bids');
-    cycle_visibility('p1-set-moves');
 }
 
 function change_bid(amount, target) {
@@ -76,13 +78,27 @@ function change_bid(amount, target) {
 }
 
 function cycle_visibility(target) {
-    target = document.getElementById(target);
     target.classList.toggle('invisible');
     target.classList.toggle('visible');
 }
 
+function set_moves() {
+    console.log('Moves setted!');
+    var targets = Array.from(document.getElementsByClassName('bidding-interface'));
+    targets.forEach( function(element) {
+        cycle_visibility(element);
+    });
+    cycle_visibility(document.getElementById('p1-place-bids'));
+    cycle_visibility(document.getElementById('p1-set-moves'));
+
+}
+
 function submit_bids() {
     console.log('Bids placed!');
-    cycle_visibility('p1-place-bids');
-    cycle_visibility('p1-set-moves');
+    var targets = Array.from(document.getElementsByClassName('bidding-interface'));
+    targets.forEach( function(element) {
+        cycle_visibility(element);
+    });
+    cycle_visibility(document.getElementById('p1-place-bids'));
+    cycle_visibility(document.getElementById('p1-set-moves'));
 }
